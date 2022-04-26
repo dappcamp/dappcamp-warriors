91e27ff28d83ff61eca895d745a17ec5bae10cd1
Lucas Janon <ljanon@gmail.com>
2022-04-26 10:24:45 -0300

    Set camp ownership on deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,11 +1,14 @@
+import { getAddress } from "../lib/addresses.helpers";
 import { deployCamp } from "./deploy-camp";
 import { deployDappCampWarriors } from "./deploy-dapp-camp-warriors";
 import { deployStaking } from "./deploy-staking";
 
 async function main() {
-  await deployCamp();
+  const campContract = await deployCamp();
   await deployDappCampWarriors();
   await deployStaking();
+
+  await campContract.transferOwnership(getAddress("staking"));
 }
 
 main()
