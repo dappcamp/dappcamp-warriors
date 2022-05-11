d7c2d38968351165bfcb7107f99b22eb0ae336ac
Harsh Agrawal <itsharshag@gmail.com>
2022-05-11 14:14:22 +0530

    Add stake and unstake buttons
    The buttons are visible to only the nftOwner
    nftOwner receives ERC20 tokens proportional to the duration of staking

--- src/components/NFT.jsx
+++ src/components/NFT.jsx
@@ -1,6 +1,54 @@
 import React from "react";
 
-export default function NFT({ imageUrl, tokenId, owner }) {
+import { useAccount, useContracts } from "../contexts";
+import { stakingContractAddr } from "../pages/_app";
+import { toastSuccessMessage } from "../utils/toast";
+
+export default function NFT({ imageUrl, tokenId, owner, isStaked, setNfts }) {
+  const account = useAccount();
+  const { dcWarriorsContract, stakingContract } = useContracts();
+  const isOwner = owner.toLowerCase() === account.toLowerCase();
+
+  const stake = async () => {
+    const approveTxn = await dcWarriorsContract.approve(
+      stakingContractAddr,
+      tokenId
+    );
+    await approveTxn.wait();
+
+    const txn = await stakingContract.stake(tokenId);
+    await txn.wait();
+    toastSuccessMessage("🦄 NFT was successfully staked!");
+    setNfts((nfts) =>
+      nfts.map((nft) => {
+        if (nft.tokenId === tokenId) {
+          return {
+            ...nft,
+            isStaked: true,
+          };
+        }
+        return nft;
+      })
+    );
+  };
+
+  const unstake = async () => {
+    const txn = await stakingContract.unstake(tokenId);
+    await txn.wait();
+    toastSuccessMessage("🦄 NFT was successfully unstaked!");
+    setNfts((nfts) =>
+      nfts.map((nft) => {
+        if (nft.tokenId === tokenId) {
+          return {
+            ...nft,
+            isStaked: false,
+          };
+        }
+        return nft;
+      })
+    );
+  };
+
   return (
     <div className="col-span-12 box-border border-0 border-solid border-neutral-200 text-sm leading-5 duration-300 sm:col-span-6 lg:col-span-3">
       <div className="h-full overflow-hidden rounded-lg border-2 border-gray-200 border-opacity-60">
@@ -25,6 +73,22 @@ export default function NFT({ imageUrl, tokenId, owner }) {
           >
             {owner}
           </p>
+          {isOwner && !isStaked && (
+            <button
+              onClick={stake}
+              className="flex rounded border-0 bg-indigo-500 py-2 px-8 text-lg text-white hover:bg-indigo-600 focus:outline-none disabled:opacity-50"
+            >
+              STAKE
+            </button>
+          )}
+          {isOwner && isStaked && (
+            <button
+              onClick={unstake}
+              className="flex rounded border-0 bg-indigo-500 py-2 px-8 text-lg text-white hover:bg-indigo-600 focus:outline-none disabled:opacity-50"
+            >
+              UNSTAKE
+            </button>
+          )}
         </div>
       </div>
     </div>
